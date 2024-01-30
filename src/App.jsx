51b88c3c05ddd1c1016d7d93b0a26747b729c9a9
Gustavo Luque <gus_luqe@live.com.ar>
2024-01-30 00:51:11 -0300

    ultimo

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,171 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import axios from 'axios';
-import WeatherCard from './components/WeatherCard';
-const APIkey = '4ed5f33dc8c2232ef32b967bf8990202';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import WeatherCard from "./components/WeatherCard";
+import "./App.css";
 
+const APIkey = "4ed5f33dc8c2232ef32b967bf8990202";
 
-function App() {  
+const translations = {
+  en: {
+    search: "Search",
+    loading: "Loading...",
+    errorFetchingWeather: "Error fetching weather data",
+    cityNotFoundError: "City not found. Please check the city name.",
+    language: "Language",
+  },
+  fr: {
+    search: "Rechercher",
+    loading: "Chargement...",
+    errorFetchingWeather:
+      "Erreur lors de la récupération des données météorologiques",
+    cityNotFoundError:
+      "Aucune donnée trouvée pour la ville. Veuillez vérifier le nom de la ville.",
+    language: "Langue",
+  },
+  es: {
+    search: "Buscar",
+    loading: "Cargando...",
+    errorFetchingWeather: "Error al obtener datos meteorológicos",
+    cityNotFoundError:
+      "No se encontraron datos para la ciudad. Por favor, verifica el nombre de la ciudad.",
+    language: "Idioma",
+  },
+};
 
+const backgrounds = {
+  soleado: "url(https://res.cloudinary.com/dfc7m5ola/image/upload/v1706585544/_7f5808e6-05cb-48d2-b8fd-68ed0bdf2a8b_dblq1s.jpg)",
+  ventoso:
+    "url(https://res.cloudinary.com/dfc7m5ola/image/upload/v1706585544/_49932833-6311-49e0-a504-e0cd2abcf78a_wf9wso.jpg)",
+  caluroso:
+    "url(https://res.cloudinary.com/dfc7m5ola/image/upload/v1706585544/_8ab8fc47-594a-48db-aa59-0d9f1aa83591_m4w6cy.jpg)",
+  frio: "url(https://res.cloudinary.com/dfc7m5ola/image/upload/v1706585544/_2ccbbe4f-e615-4eaa-9767-c53a7165591b_lxexnt.jpg)",
+};
+
+function App() {
   const [coords, setCoords] = useState();
   const [weather, setWeather] = useState();
-  const [temp,setTemp] = useState();
+  const [temp, setTemp] = useState();
   const [isLoading, setIsLoading] = useState(true);
-  const [textInput, setTextInput] = useState('');
+  const [textInput, setTextInput] = useState("");
   const [finder, setFinder] = useState();
-  
+  const [error, setError] = useState(null);
+  const [currentLanguage, setCurrentLanguage] = useState("en");
 
-  
-  
-  const success = position => {
+  const success = (position) => {
     const obj = {
       lat: position.coords.latitude,
       lon: position.coords.longitude,
-    }
+    };
     setCoords(obj);
-  }
+  };
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(success);
   }, []);
 
-
-
   useEffect(() => {
-    if (coords){
-      
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${APIkey}`;
-      axios.get(url)
-        .then(res => {
+    if (coords) {
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${APIkey}&lang=${currentLanguage}`;
+      axios
+        .get(url)
+        .then((res) => {
           const obj = {
             celsius: (res.data.main.temp - 273.15).toFixed(2),
-            fahrenheit: ((res.data.main.temp- 273.15) * (9/5) + 32).toFixed(2),
-          }
+            fahrenheit: ((res.data.main.temp - 273.15) * (9 / 5) + 32).toFixed(
+              2
+            ),
+          };
           setTemp(obj);
-          setWeather(res.data)
+          setWeather(res.data);
         })
-        .catch(err => console.log(err))
+        .catch((err) => {
+          setError(translations[currentLanguage].errorFetchingWeather);
+          console.error(err);
+        })
         .finally(() => {
-          
-            setIsLoading(false);
-                    
-        })
+          setIsLoading(false);
+        });
     }
+  }, [coords, currentLanguage]);
 
-  }, [coords]);
-
- 
-   
   useEffect(() => {
-    if (textInput) {  
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${textInput}&appid=${APIkey}`;
-      axios.get(url)
-       .then(res => {
+    if (textInput) {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${textInput}&appid=${APIkey}&lang=${currentLanguage}`;
+      axios
+        .get(url)
+        .then((res) => {
           const obj = {
             celsius: (res.data.main.temp - 273.15).toFixed(2),
-            fahrenheit: ((res.data.main.temp- 273.15) * (9/5) + 32).toFixed(2),  
-          }  
+            fahrenheit: ((res.data.main.temp - 273.15) * (9 / 5) + 32).toFixed(
+              2
+            ),
+          };
           setTemp(obj);
-          setFinder(res.data)
+          setFinder(res.data);
         })
-        .catch(err => console.log(err));
+        .catch((err) => {
+          setError(translations[currentLanguage].cityNotFoundError);
+          console.error(err);
+        });
     }
-  }, [textInput]);
-  
-  
+  }, [textInput, currentLanguage]);
 
-  
-  
+  const obtenerFondoSegunClima = () => {
+    const condicionesClimaticas = textInput ? finder : weather;
+
+    if (
+      !condicionesClimaticas ||
+      !condicionesClimaticas.main ||
+      !condicionesClimaticas.clouds
+    ) {
+      return backgrounds.soleado; // Fondo por defecto
+    }
+
+    const temperatura = condicionesClimaticas.main.temp;
+    const nubes = condicionesClimaticas.clouds.all;
+    const velocidadViento = condicionesClimaticas.wind.speed;
+
+    if (temperatura > 25 && nubes < 50) {
+      return backgrounds.caluroso; // Fondo caluroso
+    } else if (nubes > 70) {
+      return backgrounds.nublado; // Fondo nublado
+    } else if (temperatura < 10) {
+      return backgrounds.frio; // Fondo frío
+    } else if (velocidadViento > 5) {
+      return backgrounds.ventoso; // Fondo ventoso
+    } else {
+      return backgrounds.soleado; // Fondo por defecto para otras condiciones
+    }
+  };
+
+  const changeLanguage = () => {
+    const languages = ["en", "fr", "es"];
+    const currentIndex = languages.indexOf(currentLanguage);
+    const nextIndex = (currentIndex + 1) % languages.length;
+    setCurrentLanguage(languages[nextIndex]);
+  };
 
   return (
-    <div className='app'>    
-      {
-        isLoading ? (
-          <h2 className="loader">Loading...</h2>
-        ) : textInput ? (
-          <WeatherCard weather={finder} temp={temp} setTextInput={setTextInput} />
-        ) : (
-          <WeatherCard
-            weather={weather}
-            temp={temp}
-            setTextInput={setTextInput}
-          />
-        )}
+    <div className="app" style={{ backgroundImage: obtenerFondoSegunClima() }}>
+      <div className="language-menu">
+        <button className="language-button" onClick={changeLanguage}>
+          {translations[currentLanguage].language}:{" "}
+          {currentLanguage.toUpperCase()}
+        </button>
+      </div>
+      {isLoading ? (
+        <h2 className="loader">{translations[currentLanguage].loading}</h2>
+      ) : (
+        <WeatherCard
+          weather={textInput ? finder : weather}
+          temp={temp}
+          setTextInput={setTextInput}
+          error={error}
+          currentLanguage={currentLanguage}
+        />
+      )}
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,63 +1,140 @@
-import React, { useRef, useState } from 'react';
-import './styles/weatherCard.css';
+// WeatherCard.js
+import React, { useRef, useState } from "react";
+import "./styles/WeatherCard.css";
 
-const WeatherCard = ({weather, temp, setTextInput, hasError}) => {
+const WeatherCard = ({
+  weather,
+  temp,
+  setTextInput,
+  error,
+  currentLanguage,
+  changeLanguage,
+}) => {
+  const [isCelsius, setIsCelsius] = useState(true);
 
-    const [isCelsius, setIsCelsius] = useState(true);
+  const handleChange = () => {
+    setIsCelsius(!isCelsius);
+  };
 
-    
+  const city = useRef();
 
-    const handleChange = () => {
-        setIsCelsius(!isCelsius);
+  const handleForm = (event) => {
+    event.preventDefault();
+    setTextInput(city.current.value.toLowerCase().trim());
+  };
 
-    }
-
-    const city = useRef();
-  
-
-    const handleForm = event => {
-        event.preventDefault();
-        setTextInput(city.current.value.toLowerCase().trim());
-        
-    }
+  const handleReturnToTop = () => {
+    window.location.reload();
+  };
 
   return (
-    <section className='weather'>
-        <h1 className='weather__title'>Weather App</h1> 
-        <form className='weather__form' onSubmit={handleForm}>
-            <input type="text" ref={city} />
-            <button>Search</button>
-        </form>         
-        <h2 className='weather__city'>{weather?.name}, {weather?.sys.country}</h2>
-        <article className='weather__container1'>
-            <figure className='weather__fig'>
-                <img className='weather__img'
-                 src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`} alt="weather icon" />
+    <section className="weather">
+      <h1 className="weather__title">
+        {currentLanguage === "en"
+          ? "Weather App"
+          : currentLanguage === "fr"
+          ? "Application météo"
+          : "Aplicación del clima"}
+      </h1>
+      <form onSubmit={handleForm} className="weather__form">
+        <input type="text" ref={city} />
+        <button>
+          {currentLanguage === "en"
+            ? "Search"
+            : currentLanguage === "fr"
+            ? "Rechercher"
+            : "Buscar"}
+        </button>
+      </form>
+      {error && (
+        <>
+          <p style={{ color: "red" }}>{error}</p>
+          <button className="return-button" onClick={handleReturnToTop}>
+            {currentLanguage === "en"
+              ? "Home"
+              : currentLanguage === "fr"
+              ? "Accueil"
+              : "Inicio"}
+          </button>
+        </>
+      )}
+      {weather && !error && (
+        <>
+          <h2 className="weather__city">
+            {weather?.name}, {weather?.sys.country}
+          </h2>
+          <article className="weather__container1">
+            <figure className="weather__fig">
+              <img
+                className="weather__img"
+                src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`}
+                alt="weather icon"
+              />
             </figure>
             <div>
-                <h3 className='weather__clouds'>{weather?.weather[0].description}</h3>
-                <ul className='weather__info'>
-                    <li><span>Wind speed: </span><span>{weather?.wind.speed} "m/s"</span></li>
-                    <li><span>Clouds: </span><span>{weather?.clouds.all} %</span></li>
-                    <li><span>Pressure: </span><span>{weather?.main.pressure } hPa</span></li>
-                    <li><span>Humidity: </span><span>{weather?.main.humidity} %</span></li>
-                </ul>
+              <h3 className="weather__clouds">
+                {weather?.weather[0].description}
+              </h3>
+              <ul className="weather__info">
+                <li>
+                  <span>
+                    {currentLanguage === "en"
+                      ? "Wind speed:"
+                      : currentLanguage === "fr"
+                      ? "Vitesse du vent :"
+                      : "Velocidad del viento:"}{" "}
+                  </span>
+                  <span>{weather?.wind.speed} "m/s"</span>
+                </li>
+                <li>
+                  <span>
+                    {currentLanguage === "en"
+                      ? "Clouds:"
+                      : currentLanguage === "fr"
+                      ? "Nuages :"
+                      : "Nubes :"}{" "}
+                  </span>
+                  <span>{weather?.clouds.all} %</span>
+                </li>
+                <li>
+                  <span>
+                    {currentLanguage === "en"
+                      ? "Pressure:"
+                      : currentLanguage === "fr"
+                      ? "Pression :"
+                      : "Presión :"}{" "}
+                  </span>
+                  <span>{weather?.main.pressure} hPa</span>
+                </li>
+                <li>
+                  <span>
+                    {currentLanguage === "en"
+                      ? "Humidity:"
+                      : currentLanguage === "fr"
+                      ? "Humidité :"
+                      : "Humedad :"}{" "}
+                  </span>
+                  <span>{weather?.main.humidity} %</span>
+                </li>
+              </ul>
             </div>
-        </article>
-        <div className='weather__conteiner2'>
-            <h3 className='weather__temp'>{
-                isCelsius  ?
-                   temp?.celsius+'°C'
-                :
-                   temp?.fahrenheit+'°F'
-            }</h3>
-            <button className='weather__btn' onClick={handleChange}>
-                Change to {isCelsius?'°F':'°C'} 
-                </button>
-        </div>    
-    
+          </article>
+          <div className="weather__conteiner2">
+            <h3 className="weather__temp">
+              {isCelsius ? temp?.celsius + "°C" : temp?.fahrenheit + "°F"}
+            </h3>
+            <button className="weather__btn" onClick={handleChange}>
+              {currentLanguage === "en"
+                ? `Change to ${isCelsius ? "°F" : "°C"}`
+                : currentLanguage === "fr"
+                ? `Changer en ${isCelsius ? "°F" : "°C"}`
+                : `Cambiar a ${isCelsius ? "°F" : "°C"}`}
+            </button>
+          </div>
+        </>
+      )}
     </section>
-  )
-}
+  );
+};
 
 export default WeatherCard;
